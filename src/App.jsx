8e14f2899230192f86adffdc87e394d9201ac7bb
Ameya Patkar <ameyaspatkar@gmail.com>
2025-07-05 19:58:22 -0700

    Initial design for main feature (calling the API and displaying information)

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,8 @@ const ACCESS_KEY = import.meta.env.VITE_APP_ACCESS_KEY;
 
 function App() {
   const [features, setFeatures] = useState({
-    common_name: "Press Discover to Meet a New Plant!",
+    common_name: "Press Discover...!",
+    scientific_name: "...to Meet a New Plant",
     default_image: "/placeholder.avif",
     edible: "?",
     cycle: "?",
@@ -17,13 +18,44 @@ function App() {
     indoor: "?",
   });
 
-  const [banList, setBanList] = useState([]);
+  const [featureOptions, setFeatureOptions] = useState({
+    edible: ["","1"],
+    cycle: ["perennial", "annual", "biennial", "biannual"],
+    watering: ["frequent", "average", "minimum", "none"],
+    sunlight: ["full_shade", "part_shade", "sun-part_shade", "full_sun"],
+    indoor: ["","1"],
+  })
+
+  const [banList, setBanList] = useState({
+    edible: [],
+    cycle: [],
+    watering: [],
+    sunlight: [],
+    indoor: [],
+  })
+
   const [seenList, setSeenList] = useState([]);
 
   useEffect(() => {
     // The code that we want to run
     // Optional return function
   }, []) // The dependency array
+
+  const submitForm = () => {
+    e.preventDefault();
+    makeQuery();
+  }
+
+  const makeQuery = () => {
+    let url_starter = "https://";
+    let apiWebsite = "perenual.com";
+
+    let query = `https://perenual.com/api/v2/species-list?key=${ACCESS_KEY}`;
+  }
+
+  const callAPI = async (query) => {
+    const response = await axios.get(query);
+  }
 
   return (
     <div className="my-app">
--- src/Components/Display.jsx
+++ src/Components/Display.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
 
-const Display = ({features, handleChange, onSubmit, banItem}) => {
+const Display = ({features, onSubmit, banItem, unBanItem}) => {
   return (
     <div className="display">
         <h1>Wanderleaf</h1>
         <h3>Get lost in the garden of possibilities and let curiosity take root!</h3>
 
         <h2>{features.common_name}</h2>
+        <h3>{features.scientific_name}</h3>
         <div className="my-features">
             {features &&
                 Object.entries(features)
-                .filter(([key]) => !['common_name', 'default_image'].includes(key))
+                .filter(([key]) => !['common_name', 'scientific_name', 'default_image'].includes(key))
                 .map(([category, value], index) => (
                     <button>{category}: {value}</button>
             ))}
